a36310b032ea0d4117ff89aaf1793330b33ad1ee
ichmarah <ichmarah@gmail.com>
2020-02-25 00:34:59 +0100

    Remove BootstrapVue

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
