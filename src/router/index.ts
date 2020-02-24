6c8e14840090ab0e56a7ed36e0fe344cd110d652
ichmarah <ichmarah@gmail.com>
2020-02-24 14:50:29 +0100

    Create main routes Home, Categories, Random

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Categories from '../views/Categories.vue'
+import Random from '../views/Random.vue'
 
 Vue.use(VueRouter)
 
@@ -17,13 +18,9 @@ const routes = [
     component: Categories
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/random',
+    name: 'Random',
+    component: Random
   }
 ]
 
