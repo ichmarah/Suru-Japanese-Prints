e210e5986363666a4a3b294030cf5844428a011b
ichmarah <ichmarah@gmail.com>
2020-03-14 16:04:38 +0100

    Create global component BaseLoader.vue

---
+++ src/components/BaseLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <div class="d-flex justify-content-center">
+      <div :class="isLoading ? 'spinner-border' : ''" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'BaseLoader'
+})
+export default class BaseLoader extends Vue {
+  @Prop({ type: Boolean, required: true }) isLoading!: boolean
+}
+</script>
