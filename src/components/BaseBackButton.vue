a9e263b1dd6e9bc3e750743baf0213c01635a8c2
ichmarah <ichmarah@gmail.com>
2020-03-18 15:43:33 +0100

    Crete global Go Back button

---
+++ src/components/BaseBackButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p @click="goBack" class="go-back">← Go back</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default class BaseBackButton extends Vue {
+  goBack() {
+    window.history.back()
+  }
+}
+</script>
+
+<style lang="css">
+.go-back {
+  text-align: left;
+  padding-top: 10px;
+}
+</style>
