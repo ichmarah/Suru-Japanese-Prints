e569de92337af8a0bac2949be33e5d8e0d7ff341
ichmarah <ichmarah@gmail.com>
2020-03-18 15:42:38 +0100

    Add content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>About</h1>
+  <section>
+    <h3 class="top-heading">About</h3>
+    <p>
+      <strong>Suru – Japanese Prints</strong>, a website to explore prints from
+      the Japanese culture. Suru is a japanese verb that can be translated to
+      <em>to serve as</em> or <em>to print (using a wooden mold)</em>
+    </p>
+    <p>These prints are meant to inspire creatives and the curious.</p>
+    <p>Enjoy!</p>
+  </section>
 </template>
